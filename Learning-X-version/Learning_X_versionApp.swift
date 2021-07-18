71a4fcb175d24fd6a2dd1bf414dae12432ef502e
X <lizzo.alex@gmail.com>
2021-07-18 14:21:54 -0400

    Initial Commit

---
+++ Learning-X-version/Learning_X_versionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_X_versionApp.swift
+//  Learning-X-version
+//
+//  Created by X on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_X_versionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
