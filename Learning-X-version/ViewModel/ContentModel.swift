19a1b9c1dbadb69ce3629b7779d73c68fa6fc497
X <lizzo.alex@gmail.com>
2021-07-18 21:11:26 -0400

    - add data files -add assets -add modules -parsed JSON  in content model - parsed HTML in content model

--- Learning-X-version/ViewModel/ContentModel.swift
+++ Learning-X-version/ViewModel/ContentModel.swift
@@ -8,6 +8,48 @@
 import Foundation
 
 class ContentModel: ObservableObject{
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init(){
+            getLocalData()
+    }
+    
+    func getLocalData(){
+        //get a url to the JSON file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "JSON")
+        
+        //Read the file into the data object
+        do{
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            //try to decode the JSON into an array of modules
+            let jsonDecoder = JSONDecoder()
+            
+            let modules =  try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            //Assigned parsed modules to modules property
+            self.modules = modules
+            
+            
+        }catch{
+            print(error)
+        }
+        
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "HTML")
+        
+        //Read the file into the data object
+        do{
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+           
+            
+        }catch{
+            print(error)
+        }
+        
+        
+    }
 }
 
